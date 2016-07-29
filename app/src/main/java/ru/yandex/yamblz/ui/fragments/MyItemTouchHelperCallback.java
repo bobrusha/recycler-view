161273d4ab507be6b37ab423736fc4bb06d34906
Aleksandra Bobrova <bobrova.aleksand@gmail.com>
2016-07-29 21:54:51 +0300

    Added background animation when user swipe item.

--- app/src/main/java/ru/yandex/yamblz/ui/fragments/MyItemTouchHelperCallback.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/MyItemTouchHelperCallback.java
@@ -1,16 +1,26 @@
 package ru.yandex.yamblz.ui.fragments;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
+import android.view.View;
 
 /**
  * Created by Aleksandra on 29/07/16.
  */
 public class MyItemTouchHelperCallback extends ItemTouchHelper.Callback {
+    private static final String DEBUG_TAG = MyItemTouchHelperCallback.class.getName();
     private final ItemTouchHelperAdapter adapter;
+    private final Paint paint;
 
     public MyItemTouchHelperCallback(ItemTouchHelperAdapter adapter) {
         this.adapter = adapter;
+        paint = new Paint();
+        paint.setColor(Color.RED);
     }
 
     @Override
@@ -30,4 +40,18 @@ public class MyItemTouchHelperCallback extends ItemTouchHelper.Callback {
         adapter.onItemDismiss(viewHolder.getAdapterPosition());
     }
 
+
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+            View iv = viewHolder.itemView;
+            if (dX > 0) {
+                Log.d(DEBUG_TAG, "in dx > 0");
+                RectF background = new RectF((float) iv.getLeft(), (float) iv.getTop(), dX, (float) iv.getBottom());
+                paint.setAlpha((int) (255 * (dX / iv.getWidth())));
+                c.drawRect(background, paint);
+            }
+        }
+    }
 }
